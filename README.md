8c40aebb922d279863b2e2da787a6b07fc6c4483
Niklas Johansen <nikkojohansen@hotmail.com>
2020-07-07 19:00:34 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pulse Engine Game Template
+
+This repository provides a template for creating games with the [Pulse Engine](https://github.com/NiklasJohansen/PulseEngine).
+
+Instructions and examples are coming soon.
---
+++ src/main/kotlin/GameTemplate.kt
@@ -0,0 +1,33 @@
+import no.njoh.pulseengine.PulseEngine
+import no.njoh.pulseengine.modules.PulseEngineGame
+
+fun main() = PulseEngine.run(GameTemplate::class)
+
+class GameTemplate : PulseEngineGame()
+{
+    override fun onCreate()
+    {
+        engine.console.runScript("/startup.ps")
+    }
+
+    override fun onUpdate()
+    {
+
+    }
+
+    override fun onRender()
+    {
+        engine.gfx.mainSurface.drawText(
+            text = "Pulse Engine Game Template",
+            x = engine.window.width / 2f,
+            y = engine.window.height / 2f,
+            fontSize = 72f,
+            xOrigin = 0.5f
+        )
+    }
+
+    override fun onDestroy()
+    {
+
+    }
+}
