db2a3fc6dbaee18f0c41a6f6a1a4d6e98ac1a9fc
NiklasJohansen <niklasjohansen94@gmail.com>
2025-10-20 21:39:30 +0200

    Update build script and readme

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Pulse Engine - Game Template
 
 This repository provides a template project for creating games with the [Pulse Engine](https://github.com/NiklasJohansen/PulseEngine).
-Use the example code and the referenced projects bellow to get started.
+Use the example code and the referenced projects below to get started.
 
 ## Reference Projects
+- [Cæsar's Salads](https://github.com/capraconsulting/caesars-salads) is a game made for JavaZone 2025
 - [Botgang](https://github.com/NiklasJohansen/botgang) is a game where you program bots to compete in a top-down shooter
 - [Shotgang](https://github.com/NiklasJohansen/shotgang) is a top-down competitive couch coop game
 - [capra-ml-talk](https://github.com/NiklasJohansen/capra-ml-talk) is an interactive presentation of neural networks
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
-    id("edu.sc.seis.launch4j") version "3.0.6"
+    kotlin("jvm") version "2.2.20"
+    id("edu.sc.seis.launch4j") version "4.0.0"
 }
 
 group = "org.example"
@@ -31,13 +31,21 @@ tasks.jar {
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
     exclude("macos/**", "linux/**") // Exclude natives for Mac and Linux when creating Windows exe
+    exclude("*-dev*") // Exclude dev config and scripts
     manifest { attributes("Main-Class" to mainClass) }
 }
 
 launch4j {
-    bundledJrePath = "jre"
-    mainClassName = mainClass
-    outputDir = releaseBuildDir
+    bundledJrePath  = "jre"
+    mainClassName   = mainClass
+    outputDir       = releaseBuildDir
+    initialHeapSize = 1024
+    maxHeapSize     = 4096
+    jvmOptions      = listOf(
+        "-XX:+UseZGC",            // Use Z Garbage Collector for low latency
+        "-XX:SoftMaxHeapSize=2g", // 2GB target heap size to limit GC impact
+        "-XX:+DisableExplicitGC"  // Ignore System.gc() from libraries to prevent sudden stutter / frame drops
+    )
 }
 
 tasks.register<Zip>("buildWin64Release") {
@@ -46,7 +54,7 @@ tasks.register<Zip>("buildWin64Release") {
     doFirst {
         delete("$releaseBuildDir/lib")
         copy {
-            from(zipTree("jre/jre23-win64.zip"))
+            from(zipTree("jre/minimal-jre23-win64.zip"))
             into("$releaseBuildDir/jre")
         }
     }
